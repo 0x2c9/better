0a13bb305338251f3f2c20744edc87c457e4bd19
0x2c9 <grmnn@proton.me>
2023-12-29 20:21:29 +0100

    Refactor BSwitch component and add BSwitch.story.vue

--- components/BSwitch.vue
+++ components/BSwitch.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 interface IBSwitchOption {
-	value: string
+	content: string
 	label: string
 }
 interface IBSwitchProps {
@@ -10,20 +10,80 @@ interface IBSwitchProps {
 const { options } = defineProps<IBSwitchProps>()
 
 const modelValue = defineModel<IBSwitchOption>({ required: true })
-
+const selectedIndex = ref(0)
 function onOptionClick(option: IBSwitchOption) {
 	modelValue.value = option
 }
+
+const markerRef = ref<HTMLElement>()
+const btnRefs = ref<HTMLButtonElement[]>()
+
+function calculateMarkerDimensions(index: number = 0) {
+	if (!btnRefs.value?.[index] || !markerRef.value) {
+		return
+	}
+
+	selectedIndex.value = index
+	const activeButton = btnRefs.value[index]
+
+	markerRef.value.style.top = `${activeButton.offsetTop}px`
+	markerRef.value.style.left = `${activeButton.offsetLeft}px`
+	markerRef.value.style.width = `${activeButton.offsetWidth}px`
+	markerRef.value.style.height = `${activeButton.offsetHeight}px`
+}
+
+onMounted(() => {
+	calculateMarkerDimensions()
+})
+
+watch(
+	() => modelValue.value,
+	() => {
+		const index = options.findIndex((option) => option.content === modelValue.value.content)
+
+		calculateMarkerDimensions(index)
+	},
+)
+
+const throttledCalculateMarkerDimensions = useThrottleFn(() => {
+	calculateMarkerDimensions(selectedIndex.value)
+}, 500)
+
+window.addEventListener('resize', throttledCalculateMarkerDimensions)
+
+onUnmounted(() => {
+	window.removeEventListener('resize', throttledCalculateMarkerDimensions)
+})
 </script>
 
 <template>
-	<div class="w-full border border-neutral-500 rounded-2xl px-2 py-[6px] grid grid-cols-2 gap-x-3">
+	<div
+		class="w-full border border-neutral-500 rounded-2xl px-2 py-[6px] grid gap-x-3 relative"
+		:style="{
+			'grid-template-columns': `repeat(${options.length}, 1fr)`,
+		}"
+	>
+		<div
+			ref="markerRef"
+			class="absolute
+			bg-neutral-800
+			border-2
+			border-neutral-700
+			text-white
+			h-full
+			rounded-xl
+			pointer-events-none
+			-z-10
+			transition-all
+			"
+		/>
 		<button
 			v-for="option of options"
-			:key="option.value"
-			class="h-9 py-2 flex items-center justify-center rounded-xl select-none"
+			:key="option.content"
+			ref="btnRefs"
+			class="h-9 py-2 flex items-center justify-center rounded-xl select-none text-neutral-500"
 			:class="{
-				'bg-neutral-800 border-2 border-neutral-700 text-white': modelValue.value === option.value,
+				'text-white': modelValue.content === option.content,
 			}"
 			@click="onOptionClick(option)"
 		>
---
+++ stories/Form/BSwitch.story.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const mockOptions = [
+	{
+		content: 'option1',
+		label: 'Option 1',
+	},
+	{
+		content: 'option2',
+		label: 'Option 2',
+	},
+	{
+		content: 'option3',
+		label: 'Option 3',
+	},
+]
+const selectedValue = ref(mockOptions[0])
+</script>
+
+<template>
+	<Variant title="BSwitch">
+		<BSwitch
+			v-model="selectedValue"
+			:options="mockOptions"
+		/>
+
+		<pre>{{ selectedValue }}</pre>
+	</Variant>
+</template>
