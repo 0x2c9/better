aa948d379f5011af47e001b8f141bdacf873cbe8
0x2c9 <grmnn@proton.me>
2024-01-04 00:30:12 +0100

    Update BSwitch.vue styling

--- components/BSwitch.vue
+++ components/BSwitch.vue
@@ -90,7 +90,7 @@ onUnmounted(() => {
 			v-for="option of options"
 			:key="option.content"
 			ref="btnRefs"
-			class="h-9 py-2 flex items-center justify-center rounded-xl select-none text-neutral-500"
+			class="h-9 py-2 flex items-center justify-center rounded-xl select-none text-neutral-500 tabular-nums font-semibold"
 			:class="{
 				'text-white': options[selectedIndex].content === option.content,
 			}"
