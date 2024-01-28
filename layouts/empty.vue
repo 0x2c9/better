61d7eb5f0611ace11b8248d55bbb3191b3c3d956
0x2c9 <grmnn@proton.me>
2024-01-28 18:23:28 +0100

    add empty layout

---
+++ layouts/empty.vue
@@ -0,0 +1,10 @@
+<template>
+	<div
+		id="__layout"
+		class="flex h-full w-full flex-col overflow-hidden"
+	>
+		<main class="flex flex-col flex-1 overflow-y-scroll">
+			<slot class="flex-1" />
+		</main>
+	</div>
+</template>
