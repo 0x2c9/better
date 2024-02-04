80b3c6e411c3ea422c913789043056b994c78a01
0x2c9 <grmnn@proton.me>
2024-02-04 01:28:18 +0100

    add a simple error page

---
+++ error.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+defineProps<{
+	error: NuxtError
+}>()
+
+function handleError() {
+	clearError({ redirect: '/' })
+}
+</script>
+
+<template>
+	<div class="fixed inset-0 flex items-center justify-center">
+		<div class="b-box flex flex-col gap-y-4 p-4">
+			<h1 class="text-xl font-medium">
+				Something went wrong :(
+			</h1>
+			<h2 class="text-lg font-medium">
+				Error: {{ error.statusCode }}
+			</h2>
+			<BButton @click="handleError">
+				Clear errors
+			</BButton>
+		</div>
+	</div>
+</template>
