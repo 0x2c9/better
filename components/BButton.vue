beab7045133054dc687cf170c8f67006e8fccb95
0x2c9 <grmnn@proton.me>
2024-02-16 20:14:22 +0100

    style: add action-outline button variant

--- components/BButton.vue
+++ components/BButton.vue
@@ -7,7 +7,7 @@ const {
 	iconName = null,
 	iconSize = '28',
 } = defineProps<{
-	variant?: 'primary' | 'secondary' | 'action' | 'danger'
+	variant?: 'primary' | 'secondary' | 'action' | 'danger' | 'action-outline'
 	small?: boolean
 	disabled?: boolean
 	to?: string
@@ -36,6 +36,7 @@ const component = computed(() => {
 			'border-2 border-neutral-700 bg-transparent text-white active:enabled:bg-neutral-700': variant === 'secondary',
 			'border-red-700 border-2 bg-red/25 text-red active:enabled:bg-red/50': variant === 'danger',
 			'bg-primary text-neutral-950': variant === 'action',
+			'border-action border-2 bg-transparent text-primary active:enabled:bg-primary/30': variant === 'action-outline',
 			'shrink-0': isIconButton,
 			'size-12': isIconButton && !small,
 			'size-11': isIconButton && small,
