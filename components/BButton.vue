064dc84a80c29236a4c7578f3c17ef9d67a51623
0x2c9 <grmnn@proton.me>
2024-02-27 00:38:55 +0100

    fix: adapt styling for danger button so it can be used with text

--- components/BButton.vue
+++ components/BButton.vue
@@ -35,10 +35,10 @@ const component = computed(() => {
 			'bg-primary text-neutral-950 active:enabled:bg-primary/80': variant === 'primary',
 			'bg-neutral-600 text-white active:enabled:bg-neutral-700': variant === 'secondary',
 			'border-2 border-neutral-600 text-white active:enabled:bg-neutral-600/40': variant === 'outline',
-			'border-red-700 size-9 border-2 bg-red/25 text-red active:enabled:bg-red/50': variant === 'danger',
+			'border-red-700 border-2 bg-red/25 text-red active:enabled:bg-red/50': variant === 'danger',
 			'shrink-0': isIconButton,
-			'size-10': isIconButton && !small && variant !== 'danger',
-			'size-7': isIconButton && small && variant !== 'danger',
+			'size-10': isIconButton && !small,
+			'size-7': isIconButton && small,
 			'h-11 px-6 text-lg': !isIconButton && !small,
 			'h-8 px-5': !isIconButton && small,
 		}"
